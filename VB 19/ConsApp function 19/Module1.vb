01a0d5d888157225d28ffbb253fe9259bd47ced7
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:40:15 -0400

    First commit

---
+++ VB 19/ConsApp function 19/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    'funtions must return a value and you must specify that value 
+    Dim icounter = 0
+    Sub Main()
+        Console.WriteLine(GetName("bob", "smith"))
+
+
+        Console.WriteLine(counter)
+        Console.WriteLine(counter)
+        Console.WriteLine(counter)
+
+
+        Console.ReadLine()
+    End Sub
+
+    Function counter() As Integer
+        icounter += 1
+        Return icounter
+    End Function
+
+    Function GetName(ByVal first As String, ByVal last As String) As String
+        Return first & "  " & last
+
+    End Function
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual Basic 19 - Functions
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=r3WhzB6YLuo
+
+Description: Learn about Functions
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
